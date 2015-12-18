4c0a867dcef29e927c6c1aaf521e908814c60f76
Jason Coon <jasoncoon@hotmail.com>
2015-12-17 20:15:19 -0600

    Updated license, file headers.

--- PhotonMatrix.ino
+++ PhotonMatrix.ino
@@ -1,11 +1,24 @@
 /*
-* Photon Matrix by Jason Coon @jasoncoon_ at Evil Genius Labs @evilgeniuslab
-*
-* Derived from Paul Kourany's RGBPongClock: https://github.com/pkourany/RGBPongClock
-* Which was derived by RGB Pong Clock by Andrew Holmes @pongclock: https://github.com/twonk/RGBPongClock
-* Which was inspired by, and shamelessly derived from Nick's LED Projects: https://123led.wordpress.com/about
-*
-*/
+ * Photon Matrix: https://github.com/evilgeniuslabs/photonmatrix
+ * Copyright (C) 2015 Jason Coon, Evil Genius Labs
+ * 
+ * Derived from Paul Kourany's RGBPongClock: https://github.com/pkourany/RGBPongClock
+ * Which was derived by RGB Pong Clock by Andrew Holmes @pongclock: https://github.com/twonk/RGBPongClock
+ * Which was inspired by, and shamelessly derived from Nick's LED Projects: https://123led.wordpress.com/about
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 #include "PhotonMatrix.h"
 
